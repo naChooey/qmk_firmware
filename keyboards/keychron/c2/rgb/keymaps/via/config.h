e47d5bf29ed646b3f8c97b3e4ecf43023eff8cb1
naChooey <80603191+naChooey@users.noreply.github.com>
2022-07-23 02:11:32 -0700

    Add files via upload

---
+++ keyboards/keychron/c2/rgb/keymaps/via/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Enable RGB Matrix
+#define VIA_QMK_RGBLIGHT_ENABLE
+
+#define VENDOR_ID 0x05AC
+#define PRODUCT_ID 0x024F
+
+#define MANUFACTURER Keychron
+#define PRODUCT      \x43\x32 RGB
+#define DESCRIPTION  \x43\x32 RGB QMK
